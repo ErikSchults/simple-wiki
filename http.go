4d0498dd1c05877546c9ed621d9d2bf4b34a084f
erikschults <erik.schults@pipedrive.com>
2018-05-22 21:54:48 +0300

    handler creator which parses title and passes it

--- http.go
+++ http.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"html/template"
 	"log"
 	"net/http"
@@ -9,52 +8,23 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
 	log.Fatal(http.ListenAndServe(":3300", nil))
 }
 
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
-func getTitle(res http.ResponseWriter, req *http.Request) (string, error) {
-	urlParts := validPath.FindStringSubmatch(req.URL.Path)
-
-	if urlParts == nil {
-		http.NotFound(res, req)
-		return "", errors.New("Invalid Page Title")
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		match := validPath.FindStringSubmatch(req.URL.Path)
+		if match == nil {
+			http.NotFound(res, req)
+			return
+		}
+		fn(res, req, match[2])
 	}
-
-	return urlParts[2], nil
-}
-
-func viewHandler(res http.ResponseWriter, req *http.Request) {
-	title, err := getTitle(res, req)
-	if err != nil {
-		return
-	}
-
-	page, err := loadPage(title)
-	if err != nil {
-		http.Redirect(res, req, "/edit/"+title, http.StatusFound)
-		return
-	}
-
-	renderTemplate(res, "view", page)
-}
-
-func editHandler(res http.ResponseWriter, req *http.Request) {
-	title, err := getTitle(res, req)
-	if err != nil {
-		return
-	}
-
-	page, err := loadPage(title)
-	if err != nil {
-		page = &Page{Title: title}
-	}
-
-	renderTemplate(res, "edit", page)
 }
 
 func renderTemplate(res http.ResponseWriter, tmpl string, page *Page) {
@@ -70,12 +40,26 @@ func renderTemplate(res http.ResponseWriter, tmpl string, page *Page) {
 	}
 }
 
-func saveHandler(res http.ResponseWriter, req *http.Request) {
-	title, err := getTitle(res, req)
+func viewHandler(res http.ResponseWriter, req *http.Request, title string) {
+	page, err := loadPage(title)
 	if err != nil {
+		http.Redirect(res, req, "/edit/"+title, http.StatusFound)
 		return
 	}
 
+	renderTemplate(res, "view", page)
+}
+
+func editHandler(res http.ResponseWriter, req *http.Request, title string) {
+	page, err := loadPage(title)
+	if err != nil {
+		page = &Page{Title: title}
+	}
+
+	renderTemplate(res, "edit", page)
+}
+
+func saveHandler(res http.ResponseWriter, req *http.Request, title string) {
 	body := req.FormValue("body")
 	page := &Page{Title: title, Body: []byte(body)}
 	if err := page.save(); err != nil {
