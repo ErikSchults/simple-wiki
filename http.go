fbd2a9ae5c8109bf2a6c2faa90c931a48e377c71
erikschults <erik.schults@pipedrive.com>
2018-05-22 21:35:24 +0300

    allow only string/number chars in title

--- http.go
+++ http.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"log"
 	"net/http"
+	"regexp"
 )
 
-func viewHandler(res http.ResponseWriter, req *http.Request) {
-	title := req.URL.Path[len("/view/"):]
-	page, err := loadPage(title)
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":3300", nil))
+}
 
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(res http.ResponseWriter, req *http.Request) (string, error) {
+	urlParts := validPath.FindStringSubmatch(req.URL.Path)
+
+	if urlParts == nil {
+		http.NotFound(res, req)
+		return "", errors.New("Invalid Page Title")
+	}
+
+	return urlParts[2], nil
+}
+
+func viewHandler(res http.ResponseWriter, req *http.Request) {
+	title, err := getTitle(res, req)
+	if err != nil {
+		return
+	}
+
+	page, err := loadPage(title)
 	if err != nil {
 		http.Redirect(res, req, "/edit/"+title, http.StatusFound)
 		return
@@ -19,9 +44,12 @@ func viewHandler(res http.ResponseWriter, req *http.Request) {
 }
 
 func editHandler(res http.ResponseWriter, req *http.Request) {
-	title := req.URL.Path[len("/edit/"):]
-	page, err := loadPage(title)
+	title, err := getTitle(res, req)
+	if err != nil {
+		return
+	}
 
+	page, err := loadPage(title)
 	if err != nil {
 		page = &Page{Title: title}
 	}
@@ -43,21 +71,16 @@ func renderTemplate(res http.ResponseWriter, tmpl string, page *Page) {
 }
 
 func saveHandler(res http.ResponseWriter, req *http.Request) {
-	title := req.URL.Path[len("/save/"):]
+	title, err := getTitle(res, req)
+	if err != nil {
+		return
+	}
+
 	body := req.FormValue("body")
-
 	page := &Page{Title: title, Body: []byte(body)}
-
 	if err := page.save(); err != nil {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 	}
 
 	http.Redirect(res, req, "/view/"+title, http.StatusFound)
-}
-
-func main() {
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
-	log.Fatal(http.ListenAndServe(":3300", nil))
 }
